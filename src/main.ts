2ff9ed4eae7a89f4482eab2f66ebc1951525343b
Lucas Gladding <l.gladding@vehikl.com>
2020-04-21 22:56:20 -0400

    Added certs for testing.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+import * as fs from 'fs';
 import { NestFactory } from '@nestjs/core';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync('./certs/key.pem'),
+    cert: fs.readFileSync('./certs/cert.pem'),
+  };
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
   await app.listen(3000);
 }
 bootstrap();
